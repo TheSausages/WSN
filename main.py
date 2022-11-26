db0b9ba5be770a3fb9562718d3525a4a2b1cca00
kziejlo <kziejlo@gmail.com>
2022-11-26 11:38:29 +0100

    added vertex class

--- main.py
+++ main.py
@@ -92,81 +92,6 @@ end_node_name = 'end'
 #
 # print(get_all_neighbors_of('A'))
 
-
-class Graph:
-    def __init__(self):
-        self.vertices = []
-        self.edges = []
-
-    def add_vertex(self, name):
-        if self.vertices.__contains__(name):
-            print(f'{name} already added')
-        else:
-            self.vertices.append(name)
-
-    def add_edge(self, point_one, point_two, distance):
-        if not self.vertices.__contains__(point_one):
-            print(f'{point_one} does not exist')
-            return
-
-        if not self.vertices.__contains__(point_two):
-            print(f'{point_two} does not exist')
-            return
-
-        # Zabezpieczenie żeby nie było kilka takich samych?
-        self.edges.append(self.Edge(point_one, point_two, distance))
-
-    def print_graph(self):
-        print('')
-        for edge in self.edges:
-            print(f'{edge.point_one} <-> {edge.point_two}, distance: {edge.distance}')
-
-    def get_neighbors_of_vertex(self, name):
-        if not self.vertices.__contains__(name):
-            print(f'Does not contain {name}')
-            return
-
-        neighbors = []
-        for edge_element in self.edges:
-            if edge_element.point_one == name:
-                neighbors.append(edge_element.point_two)
-
-            if edge_element.point_two == name:
-                neighbors.append(edge_element.point_one)
-
-        return neighbors
-
-    class Edge:
-        def __init__(self, point_one, point_two, distance):
-            self.point_one = point_one
-            self.point_two = point_two
-            self.distance = distance
-
-
-graph = Graph()
-
-graph.add_vertex(start_node_name)
-graph.add_vertex('A')
-graph.add_vertex('B')
-graph.add_vertex('C')
-graph.add_vertex('D')
-graph.add_vertex('E')
-graph.add_vertex('F')
-graph.add_vertex(end_node_name)
-
-graph.add_edge(start_node_name, 'A', 1)
-graph.add_edge('A', 'B', 3)
-graph.add_edge('A', 'C', 3)
-graph.add_edge('B', 'D', 2)
-graph.add_edge('B', 'E', 5.5)
-graph.add_edge('C', 'E', 3)
-graph.add_edge('C', 'F', 3)
-graph.add_edge('D', end_node_name, 4)
-graph.add_edge('E', end_node_name, 3)
-graph.add_edge('F', end_node_name, 6)
-
-graph.print_graph()
-
 # prawdopodobieństwo, że uda się przesłać
 p_start_end = 1
 p_other = 0.9
@@ -183,6 +108,92 @@ e_min = 5
 t_i = 5
 t_start_end = math.inf
 
+
+
+class Vertex:
+    def __init__(self, name: str):
+        self.name = name
+        self.current_energy = e_max
+
+
+class Edge:
+    def __init__(self, point_one: Vertex, point_two: Vertex, distance: float):
+        self.point_one = point_one
+        self.point_two = point_two
+        self.distance = distance
+
+
+class Graph:
+    def __init__(self):
+        self.vertices: list[Vertex] = []
+        self.edges: list[Edge] = []
+
+    def add_vertex(self, name: str):
+        if any(vertex.name == name for vertex in self.vertices):
+            print(f'{name} already added')
+        else:
+            vertex = Vertex(name)
+            self.vertices.append(vertex)
+
+            return vertex
+
+    def add_edge(self, point_one: Vertex, point_two: Vertex, distance: float):
+        if not self.vertices.__contains__(point_one):
+            print(f'{point_one} does not exist')
+            return
+
+        if not self.vertices.__contains__(point_two):
+            print(f'{point_two} does not exist')
+            return
+
+        # Zabezpieczenie żeby nie było kilka takich samych?
+        self.edges.append(Edge(point_one, point_two, distance))
+
+    def print_graph(self):
+        print('')
+        for edge_element in self.edges:
+            print(f'{edge_element.point_one.name} <-> {edge_element.point_two.name}, distance: {edge_element.distance}')
+
+    def get_neighbors_of_vertex(self, vertex: Vertex):
+        if not self.vertices.__contains__(vertex):
+            print(f'Does not contain {vertex.name}')
+            return
+
+        neighbors = []
+        for edge_element in self.edges:
+            if edge_element.point_one == vertex:
+                neighbors.append(edge_element.point_two)
+
+            if edge_element.point_two == vertex:
+                neighbors.append(edge_element.point_one)
+
+        return neighbors
+
+
+graph = Graph()
+
+Start = graph.add_vertex(start_node_name)
+A = graph.add_vertex('A')
+B = graph.add_vertex('B')
+C = graph.add_vertex('C')
+D = graph.add_vertex('D')
+E = graph.add_vertex('E')
+F = graph.add_vertex('F')
+End = graph.add_vertex(end_node_name)
+
+graph.add_edge(Start, A, 1)
+graph.add_edge(A, B, 3)
+graph.add_edge(A, C, 3)
+graph.add_edge(B, D, 2)
+graph.add_edge(B, E, 5.5)
+graph.add_edge(C, E, 3)
+graph.add_edge(C, F, 3)
+graph.add_edge(D, End, 4)
+graph.add_edge(E, End, 3)
+graph.add_edge(F, End, 6)
+
+graph.print_graph()
+
 # OBLICZENIA
 
 # 1. Sprawdzenie warunku z odległościami - d_i < r.max
@@ -192,4 +203,5 @@ for edge in graph.edges:
 
 graph.print_graph()
 
-print(graph.get_neighbors_of_vertex('A'))
+neighbor_test = list(map(lambda vertex: vertex.name, graph.get_neighbors_of_vertex(A)))
+print(neighbor_test)
